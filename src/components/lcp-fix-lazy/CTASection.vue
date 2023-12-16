bd01b06f2628798c9f8ee85fcca6618ebc191368
Orhan Özkerçin <orhan.ozkercin@nesine.com>
2023-12-16 18:28:37 +0300

    lazy- remove high on hero-2-3

--- src/components/lcp-fix-lazy/CTASection.vue
+++ src/components/lcp-fix-lazy/CTASection.vue
@@ -18,12 +18,7 @@ const hero3Image = cld.image('hero-3').format('webp')
   <section class="bg-white dark:bg-gray-900">
     <div class="container grid grid-cols-1 gap-8 px-4 py-12 mx-auto lg:grid-cols-2">
       <div class="flex flex-col items-center max-w-lg mx-auto text-center">
-        <AdvancedImage
-          fetchpriority="high"
-          class="rounded-3xl h-44 mb-8"
-          :cldImg="hero2Image"
-          :plugins="plugins"
-        />
+        <AdvancedImage class="rounded-3xl h-44 mb-8" :cldImg="hero2Image" :plugins="plugins" />
 
         <h2 class="text-3xl font-semibold tracking-tight text-gray-800 dark:text-white">
           Web development
@@ -44,12 +39,7 @@ const hero3Image = cld.image('hero-3').format('webp')
       </div>
 
       <div class="flex flex-col items-center max-w-lg mx-auto text-center">
-        <AdvancedImage
-          fetchpriority="high"
-          class="rounded-3xl h-44 mb-8"
-          :cldImg="hero3Image"
-          :plugins="plugins"
-        />
+        <AdvancedImage class="rounded-3xl h-44 mb-8" :cldImg="hero3Image" :plugins="plugins" />
         <h2 class="text-3xl font-semibold tracking-tight text-gray-800 dark:text-white">
           App development
         </h2>
