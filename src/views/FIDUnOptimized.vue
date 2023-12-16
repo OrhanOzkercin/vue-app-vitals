460f3f075cb6166a739ac23d8d4c7c5c4aeceb5f
Orhan Özkerçin <orhan.ozkercin@nesine.com>
2023-12-16 20:59:14 +0300

    fid - take from lazy

--- src/views/FIDUnOptimized.vue
+++ src/views/FIDUnOptimized.vue
@@ -1,12 +1,12 @@
 <script setup>
-import CTASection from '../components/initial/CTASection.vue'
+import CTASection from '../components/lcp-fix-lazy/CTASection.vue'
 import FeaturesSection from '../components/initial/FeaturesSection.vue'
 import FooterSection from '../components/initial/FooterSection.vue'
-import HeaderSection from '../components/initial/HeaderSection.vue'
-import PortfolioSection from '../components/initial/PortfolioSection.vue'
+import HeaderSection from '../components/lcp-fix-lazy/HeaderSection.vue'
+import PortfolioSection from '../components/lcp-fix-lazy/PortfolioSection.vue'
+import TeamSection from '../components/lcp-fix-lazy/TeamSection.vue'
+import TestimonialSection from '../components/lcp-fix-lazy/TestimonialSection.vue'
 import LoadScript from '../components/initial/LoadScript.vue'
-import TeamSection from '../components/initial/TeamSection.vue'
-import TestimonialSection from '../components/initial/TestimonialSection.vue'
 import BazingaGif from '../components/initial/BazingaGif.vue'
 </script>
 
@@ -18,7 +18,7 @@ import BazingaGif from '../components/initial/BazingaGif.vue'
   <div class="ad-container mb-10">
     <div id="responsive-ad" class="ad-slot"></div>
   </div>
-  <hr class="mb-10"/>
+  <hr class="mb-10" />
   <BazingaGif />
   <TeamSection />
   <TestimonialSection />
